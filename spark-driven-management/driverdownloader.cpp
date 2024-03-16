b5dde6a64b7f04c4f86d9287e44776c1e252d220
vmomenv <vmomenv@gmail.com>
2024-03-16 11:29:35 +0800

    更新

--- spark-driven-management/driverdownloader.cpp
+++ spark-driven-management/driverdownloader.cpp
@@ -1,6 +1,11 @@
 #include "driverdownloader.h"
+#include "driverdownloader.h"
 
+#include <QJsonArray>
+#include <QJsonDocument>
 #include <QListWidget>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
 #include <QProcess>
 #include <QPushButton>
 #include <QUrlQuery>
@@ -36,4 +41,51 @@ void DriverDownloader::executeCommand()
     }
     qDebug()<<deviceIDs;
 
+}
+void DriverDownloader::fetchDriverInfo()
+{
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+
+    for (const QString &deviceId : deviceIDs) {
+        QUrl apiUrl("http://127.0.0.1:8000/api/FindFilesByHardwareId/");
+        QUrlQuery query;
+        query.addQueryItem("driver_type", "pci");
+        query.addQueryItem("device_id", deviceId);
+        apiUrl.setQuery(query);
+
+        QNetworkRequest request(apiUrl);
+        QNetworkReply *reply = manager->get(request);
+
+        connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+            onDriverInfoReceived(reply);
+        });
+    }
+}
+void DriverDownloader::onDriverInfoReceived(QNetworkReply *reply)
+{
+    if (reply->error() == QNetworkReply::NoError) {
+        QByteArray data = reply->readAll();
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(data);
+        QJsonArray driverInfoArray = jsonDoc.array();
+
+        // 追加到中间变量
+        intermediateJsonArray.append(driverInfoArray);
+
+        // 检查是否有数据，如果有则发送信号
+        if (!driverInfoArray.isEmpty()) {
+            emit driverInfoReceived(intermediateJsonArray);
+        }
+    } else {
+        qDebug() << "Network error:" << reply->errorString();
+    }
+
+    reply->deleteLater();
+}
+void DriverDownloader::processDriverInfo(const QJsonArray &driverInfoArray)
+{
+    // 处理驱动信息，你可以在这里做进一步的操作
+    qDebug() << "Received driver info:" << driverInfoArray;
+
+    // 清空中间变量，准备下一次的数据
+    intermediateJsonArray = QJsonArray();
 }
--- spark-driven-management/driverdownloader.h
+++ spark-driven-management/driverdownloader.h
@@ -3,14 +3,23 @@
 
 #include <QWidget>
 #include<QDebug>
+#include <QNetworkReply>
+#include <QJsonArray>
 class DriverDownloader : public QWidget
 {
     Q_OBJECT
 public:
     explicit DriverDownloader(QWidget *parent = nullptr);
     void executeCommand();
+    void fetchDriverInfo();
+    void onDriverInfoReceived(QNetworkReply *reply);
+    void processDriverInfo(const QJsonArray &driverInfoArray);
+private:
     QStringList deviceIDs;
+    QJsonArray intermediateJsonArray;
+    QNetworkAccessManager *networkManager;
 signals:
+    void driverInfoReceived(const QJsonArray &driverInfoArray);
 
 public slots:
 };
